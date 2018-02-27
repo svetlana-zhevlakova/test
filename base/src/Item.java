bf5d164cb1de5336a3fb6bb2baa38fb960477e11
Svetlana Zhevlakova <svettkins@gmail.com>
2018-02-27 22:10:21 +0300

    item commit

---
+++ base/src/Item.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Svetlana on 27.02.2018.
+ */
+public class Item {
+
+    private long id;
+    private String name;
+
+    public Item(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
