ef3ccaf07035ed046e086928632142751e48609f
Svetlana Zhevlakova <svettkins@gmail.com>
2018-02-27 22:29:08 +0300

    added customer

--- base/src/Main.java
+++ base/src/Main.java
@@ -3,30 +3,7 @@
  */
 public class Main {
 
-    private String name;
-    private int age;
-
-    public Main() {
-    }
-
-    public Main(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
+    public static void main(String[] args) {
+        Customer customer = new Customer("Mark", 24);
     }
 }
